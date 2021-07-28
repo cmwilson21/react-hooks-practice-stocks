fec37bf64d08bbd029a40067d199cfe18e798be1
Courtney Wilson <cmwilson132@gmail.com>
2021-07-28 15:11:30 -0500

    mostly completed

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([])
+  const [clickedStocks, setClickedStocks] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then(resp => resp.json())
+      .then(data => setStocks(data))
+  }, [])
+
+  const handleClick = (stock) => {
+    setClickedStocks([...clickedStocks, stock])
+    console.log(stock)
+  }
+
+  const removeStock = (stock) => {
+    const newList = clickedStocks.filter((clickedStock) => stock.id !== clickedStock.id);
+    setClickedStocks(newList)
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer handleClick={handleClick} stocks={stocks} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={clickedStocks} remove={removeStock}/>
         </div>
       </div>
     </div>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({remove, stocks}) {
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {stocks.map(( stock ) => <Stock handleClick={remove} key={stock.id} stock={stock}  />)}
     </div>
   );
 }
